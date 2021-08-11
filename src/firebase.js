b48fe9846bd36b60311ef9d99937604dba3999c8
Sanyog Changmai <sanyogchangmai29@gmail.com>
2021-08-11 15:25:01 +0530

    Removed private stuffs

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,6 @@ const app = firebase.initializeApp({
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID
-    // apiKey: process.env.REACT_APP_API_KEY,
-    // authDomain: "moments-dev-f6b59.firebaseapp.com",
-    // projectId: "moments-dev-f6b59",
-    // storageBucket: "moments-dev-f6b59.appspot.com",
-    // messagingSenderId: "685446575335",
-    // appId: "1:685446575335:web:ae90e7535ee990b0c11e42"
 })
 
 export const auth = app.auth();
